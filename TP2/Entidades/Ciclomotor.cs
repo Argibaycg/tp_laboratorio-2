fd9d82ca6209c1fffa64cd5703af1d62c58f7a3c
Argibay Gaston <argibaycg@gmail.com>
2020-10-04 20:55:11 -0300

    Agrego TP2 al repositorio

---
+++ TP2/Entidades/Ciclomotor.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Ciclomotor : Vehiculo
+    {
+
+        /// <summary>
+        /// Constructor de la clase ciclomotor que llama al constructor base vehiculo
+        /// </summary>
+        /// <param name="marca">marca de la moto</param>
+        /// <param name="chasis">chasis de la moto</param>
+        /// <param name="color">color de la moto</param>
+        public Ciclomotor(EMarca marca, string chasis, ConsoleColor color) : base(chasis, marca, color)
+        {
+        }
+
+        /// <summary>
+        /// Las motos son chicas
+        /// </summary>
+        protected override ETamanio Tamanio
+        {
+            get
+            {
+                return ETamanio.Chico;
+            }
+        }
+
+        public override string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("CICLOMOTOR");
+            sb.AppendLine(base.Mostrar());
+            sb.AppendFormat("TAMAÑO : {0}", this.Tamanio);
+            sb.AppendLine("");
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ TP2/Entidades/Sedan.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Drawing;
+
+namespace Entidades
+{
+    public class Sedan : Vehiculo
+    {
+        public enum ETipo { CuatroPuertas, CincoPuertas }
+
+        ETipo tipo;
+
+        /// <summary>
+        /// Por defecto, TIPO será CuatroPuertas
+        /// </summary>
+        /// <param name="marca"></param>
+        /// <param name="chasis"></param>
+        /// <param name="color"></param>
+        public Sedan(EMarca marca, string chasis, ConsoleColor color)
+            : base(chasis, marca, color)
+        {
+            tipo = ETipo.CuatroPuertas;
+        }
+
+        /// <summary>
+        /// Constructor que recibe ademas de lo que pide la base, el tipo y lo asigna.
+        /// </summary>
+        /// <param name="marca">marca del automovil</param>
+        /// <param name="chasis">chasis del automovil</param>
+        /// <param name="color">color del automovil</param>
+        public Sedan(EMarca marca, string chasis, ConsoleColor color, ETipo tipo) : this(marca, chasis, color)
+        {
+            this.tipo = tipo;
+        }
+
+        /// <summary>
+        /// Los automoviles son medianos
+        /// </summary>
+        protected override ETamanio Tamanio
+        {
+            get
+            {
+                return ETamanio.Mediano;
+            }
+        }
+
+        public override sealed string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("SEDAN");
+            sb.AppendLine(base.Mostrar());
+            sb.AppendFormat("TAMAÑO : {0}", this.Tamanio);
+            sb.AppendLine("TIPO : " + this.tipo);
+            sb.AppendLine("");
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+    }
+}
